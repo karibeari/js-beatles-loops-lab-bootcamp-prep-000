576a2238fd0f97f92cd22e3684daf463115acfd7
Kari Ferenczy <klferenczy@gmail.com>
2019-01-27 02:55:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+
+  for(var counter = 0; counter < musicians.length; counter++){
+    array.push(musicians[counter] + "plays" + instruments[counter]);
+  }
+}
