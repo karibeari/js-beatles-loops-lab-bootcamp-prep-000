4a014406ba0318d953eb4aa62a442e82953d8d67
Kari Ferenczy <klferenczy@gmail.com>
2019-01-27 02:55:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for(var counter = 0; counter < musicians.length; counter++){
-    array.push(musicians[counter] + "plays" + instruments[counter]);
+    array.push(musicians[counter] + " plays " + instruments[counter]);
   }
   return array;
 }
